5e5faff919e5526f6d2593f750cd13c6766d5e40
Isaac Shivvers <ishivvers@berkeley.edu>
2013-01-07 03:54:36 -0800

    Major edits --- all three methods now functional, and everything runs off of local MongoDB database.
    TO DO:
    - clean up front page (turn ALL-SKY ... into header only)
    - add info
    - add detailed description page, with some new quality plots
    - make contacts page
    - is ra,dec checking on uploads page broken?
    - add waiting image to uploads page

--- web/app/views.py
+++ web/app/views.py
@@ -9,7 +9,7 @@ from my_code import get_SEDs as gs #my library written to predict SEDs
 
 '''
 TO DO:
- - if using SDSS+2MASS, do not return usnob mags
+ - make mode 3 work
  - include link to simbad for each source
 '''
 
@@ -52,7 +52,8 @@ def show_upload():
         # see which mode we're in
         mode = int(request.form['mode'])
         if mode == 1:
-            # test whether ra,dec, and fs pass muster:
+            # produce catalog for single field
+            #  first test whether ra,dec, and fs pass muster:
             data = ra, dec, fs = map(float, [request.form['RA'], request.form['DEC'], request.form['FS']])
             if not (0. < ra < 360.) and (-90. < dec < -90) and (0. < fs < 7200.):
                 return render_template( "upload.html", feedback="Make sure you've entered valid parameters!")
@@ -61,6 +62,7 @@ def show_upload():
             coll.insert( {"entry":"mode", "mode":mode} )
             coll.insert( {"entry":"search_field", "ra":ra, "dec":dec, "fs":fs} )
         elif mode == 2:
+            # produce matched catalog
             source_file = request.files["source_file"]
             if source_file and allowed_file(source_file.filename):
                 # try to parse with numpy
@@ -77,7 +79,28 @@ def show_upload():
             coll = create_collection()
             coll.insert( {"entry":"mode", "mode":mode} )
             for row in data:
-                coll["requested_coords"].insert( {"ra":row[0], "dec":row[1] })
+                coll["requested_sources"].insert( {"ra":row[0], "dec":row[1] })
+        elif mode == 3:
+            # produce matched catalog and zeropoint estimate
+            source_file = request.files["source_file"]
+            if source_file and allowed_file(source_file.filename):
+                # try to parse with numpy
+                try:
+                    source_file.save( app.config['UPLOAD_FOLDER'] + 'tmp_source.txt' )
+                    data = np.loadtxt( app.config['UPLOAD_FOLDER'] + 'tmp_source.txt' )
+                except:
+                    return render_template( "upload.html", feedback="File upload failed! Make sure the file " + \
+                                                    "is a .txt file readable with numpy.loadtxt()!")
+            else:
+                return render_template( "upload.html", feedback="File upload failed! Make sure the file " + \
+                                                "is a .txt file readable with numpy.loadtxt()!")
+            # if all's good, create the collection and populate it
+            coll = create_collection()
+            coll.insert( {"entry":"mode", "mode":mode} )
+            coll.insert( {"entry":"passband", "passband":request.form["band"]} )
+            for row in data:
+                coll["requested_sources"].insert( {"ra":row[0], "dec":row[1], "inst_mag":row[2] })
+            
         return render_template( "upload.html", mode=mode, data=data[:5] )
     
 ## show_upload() helper functions
@@ -85,7 +108,7 @@ def create_collection():
     # create a new collection in the database, using the unix time and a random integer
     #  to ensure a datable (yet unique) collection.  Remove these with cronjob!
     sid = str(time()).split('.')[0]+'_'+str(np.random.randint(9999))
-    session['sid'] = sid
+    session['sid'] = sid #I use the flask.sessions interface to keep track of data across requests
     return DB[sid]
     
 ALLOWED_EXTENSIONS = set(['txt','dat'])
@@ -102,6 +125,24 @@ def show_results():
     coll = DB[ session['sid'] ]
     # case out the three different modes
     mode = coll.find_one( {"entry":"mode"} )['mode']
+    
+    # first, test to see whether we've already built a database, and simply display it
+    if coll['data'].find_one():
+        curs = coll['data'].find()
+        model_indices, coords = [], []
+        for i in range(curs.count()):
+            obj = curs.next()
+            model_indices.append( obj["models"] )
+            coords.append( obj["coords"] )
+        if (mode == 1) or (mode == 2):
+            render_template( "results12.html", spec_ids=map(int, model_indices), coords=coords )
+        elif mode == 3:
+            band = coll.find_one( {"entry":"passband"} )["passband"]
+            zp_est = coll.find_one( {"entry":"zeropoint_estimate"})["zp"]
+            return render_template( "results3.html", spec_ids=map(int, model_indices), coords=coords,\
+                                        zp=round(zp_est,2), band=band )
+            
+    
     if mode == 1:
         search_field = coll.find_one( {"entry":"search_field"})
         ra = search_field['ra']
@@ -111,12 +152,12 @@ def show_results():
         model_indices, errors = zip(*models)
         for i in range(len(seds)):
             coll['data'].insert( {"index":i, "sed":seds[i].tolist(), "errors":errors[i].tolist(),\
-                                    "mode":modes[i], "coords":coords[i].tolist()} )
-        return render_template( "results.html", spec_ids=map(int, model_indices), coords=coords )
+                                    "mode":modes[i], "coords":coords[i].tolist(), "models":int(model_indices[i])} )
+        return render_template( "results12.html", spec_ids=map(int, model_indices), coords=coords )
         
     elif mode == 2:
         requested_coords = []
-        curs = coll['requested_coords'].find()
+        curs = coll['requested_sources'].find()
         for i in range(curs.count()):
             obj = curs.next()
             requested_coords.append( [obj['ra'], obj['dec']] )
@@ -131,14 +172,50 @@ def show_results():
         i = 0
         for match in matches:
             if match != None:
-                coll['data'].insert( {"index":i, "sed":seds[match].tolist(), "errors":errors[match].tolist(),
-                                        "mode":modes[match], "coords":coords[match].tolist()} )
+                coll['data'].insert( {"index":i, "sed":seds[match].tolist(), "errors":errors[match].tolist(),\
+                                        "mode":modes[match], "coords":coords[match].tolist(), "models":int(model_indices[match])} )
                 out_model_indices.append( model_indices[match] )
                 out_coords.append( coords[match] )
                 i +=1
-        return render_template( "results.html", spec_ids=map(int, out_model_indices), coords=out_coords )
-
-
+        return render_template( "results12.html", spec_ids=map(int, out_model_indices), coords=out_coords )
+    
+    elif mode == 3:
+        band = coll.find_one( {"entry":"passband"} )["passband"]
+        requested_coords, inst_mags = [],[]
+        curs = coll['requested_sources'].find()
+        for i in range(curs.count()):
+            obj = curs.next()
+            requested_coords.append( [obj['ra'], obj['dec']] )
+            inst_mags.append( obj['inst_mag'] )
+        requested_coords = np.array(requested_coords) #put into numpy array for sake of functions below
+        
+        center, size = gs.find_field( requested_coords )
+        coords, seds, models, modes = gs.catalog( center, size, object_coords=requested_coords, return_models=True )
+        model_indices, errors = zip(*models)
+        # match requested coords to returned sources
+        matches = gs.identify_matches( requested_coords, coords)
+        out_coords, out_model_indices, zp = [],[],[]
+        i = 0
+        for j,match in enumerate(matches):
+            if match != None:
+                coll['data'].insert( {"index":i, "sed":seds[match].tolist(), "errors":errors[match].tolist(),\
+                                        "mode":modes[match], "coords":coords[match].tolist(), "models":int(model_indices[match])} )
+                out_model_indices.append( model_indices[match] )
+                out_coords.append( coords[match] )
+                observed = seds[match][ ALL_FILTERS==band ][0]
+                instrumental = inst_mags[j]
+                zp.append( observed-instrumental )
+                i +=1
+        # finally, calculate the zeropoint estimates for real
+        zp = np.array(zp)
+        zp_cut = zp[ np.abs(zp-np.mean(zp)) < 2*np.std(zp) ]
+        zp_est = np.mean(zp_cut)
+        coll.insert( {"entry":"zeropoint_estimate", "zp":zp_est} )
+        for val in zp_cut:
+            coll['zeropoints'].insert( {"zp":val} )
+        return render_template( "results3.html", spec_ids=map(int, out_model_indices), coords=out_coords,\
+                                    zp=zp_est, band=band )
+        
 
 
 #######################################################################
@@ -190,17 +267,25 @@ def serve_sed_flams():
     coll = DB[ session['sid'] ]
     curs = coll['data'].find_one( {"index":sed_index} )
     sed_mags = curs["sed"]
+    mode = curs["mode"]
+    if mode == 1:
+        #USNOB+2MASS
+        modeled = ['y']*6 + ['n']*5
+    else:
+        #SDSS+2MASS
+        modeled = ['n']*5 + ['y']*3 + ['n']*3
     sed_flam = mag2flam( sed_mags, ALL_FILTERS )
     
-    # push wavelengths (A) and flam into json-able format
-    json_list = [{'x': FILTER_PARAMS[ALL_FILTERS[i]][0], 'y': sed_flam[i], 'name': ALL_FILTERS[i]} for i in range(len(sed_flam))]
+    # push everything into json-able format
+    json_list = [{'x': FILTER_PARAMS[ALL_FILTERS[i]][0], 'y': sed_flam[i], 'name': ALL_FILTERS[i],\
+                    'modeled':modeled[i]} for i in range(len(sed_flam))]
     return Response(json.dumps( json_list ), mimetype='application/json')
 
 
 @app.route('/servemags', methods=['GET'])
 def serve_sed_mags():
     '''
-    Loads magnitudes (obs and modeled) from database created by upload, returns FLAM.
+    Loads magnitudes (obs and modeled) from database created by results page, returns FLAM.
     Needs database key (given as url?key=value).
     '''
     sed_index = int(request.args.get('index',''))    
@@ -249,6 +334,22 @@ def serve_full_catalog():
     response.headers['Content-Disposition'] = 'attachment; filename=catalog.txt'
     return response
 
+
+@app.route('/servezp', methods=['GET'])
+def serve_zeropoints():
+    '''
+    Loads and returns zeropoint estimates as inserted into the database by results page.
+    Needs database key (given as url?key=value).
+    '''
+    coll = DB[ session['sid'] ]
+    curs = coll['zeropoints'].find()
+    json_list = []
+    for i in range(curs.count()):
+        obj = curs.next()
+        json_list.append( {'zp': obj['zp']} )
+    return Response(json.dumps( json_list ), mimetype='application/json')
+    
+    
     
 #######################################################################
 def mag2flam( magnitudes, bands ):

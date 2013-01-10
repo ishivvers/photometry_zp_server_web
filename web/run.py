d3f38511e003453dc9cac6da210fb4d437c3b1c8
Isaac Shivvers <ishivvers@gmail.com>
2013-01-09 18:12:54 -0800

    moving run.py to linux machine

--- web/run.py
+++ web/run.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+#!/usr/local/bin/Enthought/epd-7.3-1-rh5-x86_64/bin/python
+
 from app import app
-#from app import app as application
+from app import app as application
 if __name__ == '__main__':
     app.run(debug = True)

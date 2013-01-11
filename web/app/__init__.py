9fa92e7e223e0057372ace4911f87570eb3c4002
Isaac Shivvers <ishivvers@gmail.com>
2013-01-10 17:07:51 -0800

    lots of file renaming

--- web/app/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from flask import Flask
-app = Flask(__name__) # the main flask object
-
-app.config['UPLOAD_FOLDER'] = '/Users/isaac/Working/code/photo_zp_server/web/tmp/'
-app.config['SECRET_KEY'] = 'random!modnar'
-app.config['SPECTRA_FOLDER'] = '/Users/isaac/Working/code/photo_zp_server/web/app/static/spectra/'
-
-from app import views # the site views, defined in views.py
--- web/run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/Enthought/epd-7.3-1-rh5-x86_64/bin/python
-
-from app import app
-from app import app as application
-if __name__ == '__main__':
-    app.run(debug = True)

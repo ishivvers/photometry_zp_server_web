45aae2ab5b6fdd2f12ef0ebf68260e859021e7b8
Isaac Shivvers <ishivvers@berkeley.edu>
2013-02-11 14:23:37 -0800

    built basic api response, seems to be working pretty well.

--- PhotoZPE/views.py
+++ PhotoZPE/views.py
@@ -10,18 +10,23 @@ from my_code import get_SEDs as gs #my library written to predict SEDs
 '''
 TO DO:
  - include link to simbad for each source
+ - include chosen stellar type in image
+ - put sources on image of sky intead of list
+ - put in image stuff requested by josh
+ - update info page (api, description of zp)
+ - for both modes 2&3, have page return all requested
+    sources, with phot only filled in for those that are matched.
+ - if given a query_ID, have result page display results for that query
+ - have nice error pages
+ - update upload page to have overlay instead of append (OR JUST REPLACE HERO DIV!!)
+ - set browse/upload button to my own design
 '''
 
 ########################################################################
 # The below can stay as global variables, since they don't change across threads
 #  Central wavelength and zeropoint for all filters (zp in erg/s/cm^2/A )
-FILTER_PARAMS =  {'u': (3551., 8.5864e-9), 'g': (4686., 4.8918e-9),
-                  'r': (6165., 2.8473e-9), 'i': (7481., 1.9367e-9),
-                  'z': (8931., 1.3564e-9), 'y': (10091., 1.0696e-9),
-                  'B': (4400., 6.6000e-9),  'R':(6500., 2.1900e-9),
-                  'J':(12350., 3.1353e-10), 'H':(16620., 1.1121e-10),
-                  'K':(21590., 4.2909e-11)}
-ALL_FILTERS = np.array(['u','g','r','i','z','y','B','R','J','H','K'])
+FILTER_PARAMS =  gs.FILTER_PARAMS
+ALL_FILTERS = np.array(gs.ALL_FILTERS)
 
 try:
     #MODELS = np.load( '/var/www/photozpe/my_code/all_models_P.npy','r' )
@@ -68,8 +73,6 @@ def show_upload():
         elif mode == 2:
             # produce matched catalog
             source_file = request.files["source_file"]
-            print source_file
-
             if source_file and allowed_file(source_file.filename):
                 # try to parse with numpy
                 try:
@@ -108,7 +111,8 @@ def show_upload():
                 coll["requested_sources"].insert( {"ra":row[0], "dec":row[1], "inst_mag":row[2] })
             
         return render_template( "upload.html", mode=mode, data=data[:5] )
-    
+
+
 ## show_upload() helper functions
 def create_collection():
     # create a new collection in the database, using the unix time and a random integer
@@ -116,7 +120,8 @@ def create_collection():
     sid = str(time()).split('.')[0]+'_'+str(np.random.randint(9999))
     session['sid'] = sid #I use the flask.sessions interface to keep track of data across requests
     return DB[sid]
-    
+
+
 ALLOWED_EXTENSIONS = set(['txt','dat', 'cat'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -131,7 +136,7 @@ def show_results():
     coll = DB[ session['sid'] ]
     # case out the three different modes
     mode = coll.find_one( {"entry":"mode"} )['mode']
-
+    
     # first, test to see whether we've already built a database, and simply display it
     if coll['data'].find_one() != None:
         curs = coll['data'].find()
@@ -148,19 +153,18 @@ def show_results():
             zp_mad = coll.find_one( {"entry":"zeropoint_estimate"})["mad"]
             return render_template( "results3.html", spec_ids=map(int, model_indices[:max_disp]), coords=coords[:max_disp],\
                                         zp=round(zp_est,2), mad=round(zp_mad,2), band=band )
-            
     
     if mode == 1:
         search_field = coll.find_one( {"entry":"search_field"})
         ra = search_field['ra']
         dec = search_field['dec']
         fs = search_field['fs']
-        coords, seds, models, errors, modes = gs.catalog( (ra,dec), (fs, fs), return_models=True ) #square box of size fs
-        for i in range(len(seds)):
-            coll['data'].insert( {"index":i, "sed":seds[i].tolist(), "errors":errors[i].tolist(),\
-                                    "mode":modes[i], "coords":coords[i].tolist(), "models":int(models[i])} )
-        return render_template( "results12.html", spec_ids=map(int, models[:max_disp]), coords=coords[:max_disp] )
-        
+        cat = gs.catalog( (ra,dec), fs ) #square box of size fs
+        for i in range(len(cat.SEDs)):
+            coll['data'].insert( {"index":i, "sed":cat.SEDs[i].tolist(), "errors":cat.full_errors[i].tolist(),\
+                                    "mode":cat.modes[i], "coords":cat.coords[i].tolist(), "models":int(cat.models[i])} )
+        return render_template( "results12.html", spec_ids=map(int, cat.models[:max_disp]), coords=cat.coords[:max_disp] )
+    
     elif mode == 2:
         requested_coords = []
         curs = coll['requested_sources'].find()
@@ -170,17 +174,18 @@ def show_results():
         requested_coords = np.array(requested_coords) #put into numpy array for sake of functions below
         
         center, size = gs.find_field( requested_coords )
-        coords, seds, models, errors, modes = gs.catalog( center, size, object_coords=requested_coords, return_models=True )
+        cat = gs.catalog( center, max(size) )
+        
         # match requested coords to returned sources
-        matches = gs.identify_matches( requested_coords, coords)
+        matches = gs.identify_matches( requested_coords, cat.coords)
         out_coords, out_model_indices = [],[]
         i = 0
         for match in matches:
             if match != None:
-                coll['data'].insert( {"index":i, "sed":seds[match].tolist(), "errors":errors[match].tolist(),\
-                                        "mode":modes[match], "coords":coords[match].tolist(), "models":int(models[match])} )
-                out_model_indices.append( models[match] )
-                out_coords.append( coords[match] )
+                coll['data'].insert( {"index":i, "sed":cat.SEDs[match].tolist(), "errors":cat.full_errors[match].tolist(),\
+                                        "mode":cat.modes[match], "coords":cat.coords[match].tolist(), "models":int(cat.models[match])} )
+                out_model_indices.append( cat.models[match] )
+                out_coords.append( cat.coords[match] )
                 i +=1
         return render_template( "results12.html", spec_ids=map(int, out_model_indices[:max_disp]), coords=out_coords[:max_disp] )
     
@@ -196,14 +201,16 @@ def show_results():
         inst_mags = np.array(inst_mags)
         
         center, size = gs.find_field( requested_coords )
-        coords, seds, models, errors, modes = gs.catalog( center, size, object_coords=requested_coords, return_models=True )
+        cat = gs.catalog( center, max(size) )
         
         # pull out only the band we care about
-        mod_mags = np.array([ sss[ ALL_FILTERS==band ][0] for sss in seds ])
+        print cat.SEDs[0]
+        mod_mags = np.array([ sss[ ALL_FILTERS==band ] for sss in cat.SEDs ])
+        print mod_mags[0]
         
         # calculate zeropoint
-        median_zp, mad_zp, matches, all_zps = gs.calc_zeropoint( requested_coords, coords, inst_mags, mod_mags, return_zps=True)
-        coll.insert( {"entry":"zeropoint_estimate", "zp":median_zp, "mad":mad} )
+        median_zp, mad_zp, matches, all_zps = gs.calc_zeropoint( requested_coords, cat.coords, inst_mags, mod_mags, return_zps=True)
+        coll.insert( {"entry":"zeropoint_estimate", "zp":median_zp, "mad":mad_zp} )
         for val in all_zps:
             coll['zeropoints'].insert( {"zp":val} )
         # put into db
@@ -211,15 +218,14 @@ def show_results():
         i = 0
         for j,match in enumerate(matches):
             if match != None:
-                coll['data'].insert( {"index":i, "sed":seds[match].tolist(), "errors":errors[match].tolist(),\
-                                        "mode":modes[match], "coords":coords[match].tolist(), "models":int(models[match])} )
-                out_model_indices.append( models[match] )
-                out_coords.append( coords[match] )
+                coll['data'].insert( {"index":i, "sed":cat.SEDs[match].tolist(), "errors":cat.full_errors[match].tolist(),\
+                                        "mode":cat.modes[match], "coords":cat.coords[match].tolist(), "models":int(cat.models[match])} )
+                out_model_indices.append( cat.models[match] )
+                out_coords.append( cat.coords[match] )
                 i +=1
-
+        
         return render_template( "results3.html", spec_ids=map(int, out_model_indices[:max_disp]), coords=out_coords[:max_disp],\
                                     zp=round(median_zp,2), mad=round(mad_zp,2), band=band )
-        
 
 
 #######################################################################
@@ -259,7 +265,7 @@ def serve_spectrum():
     
     # push data into a json-able format: a list of dictionaries
     json_list = [{'x': wl[i], 'y': spec[i]} for i in range(len(wl))]
-    return Response(json.dumps( json_list ), mimetype='application/json')
+    return Response(json.dumps( json_list, indent=2 ), mimetype='application/json')
 
 
 @app.route('/serveflams', methods=['GET'])
@@ -284,7 +290,7 @@ def serve_sed_flams():
     # push everything into json-able format
     json_list = [{'x': FILTER_PARAMS[ALL_FILTERS[i]][0], 'y': sed_flam[i], 'name': ALL_FILTERS[i],\
                     'modeled':modeled[i]} for i in range(len(sed_flam))]
-    return Response(json.dumps( json_list ), mimetype='application/json')
+    return Response(json.dumps( json_list, indent=2 ), mimetype='application/json')
 
 
 @app.route('/servemags', methods=['GET'])
@@ -309,7 +315,7 @@ def serve_sed_mags():
     # push everything into json-able format
     json_list = [{'x': FILTER_PARAMS[ALL_FILTERS[i]][0], 'y': sed_mags[i], 'err': sed_errs[i], \
                  'modeled': modeled[i], 'name': ALL_FILTERS[i]} for i in range(len(sed_mags))]
-    return Response(json.dumps( json_list ), mimetype='application/json')
+    return Response(json.dumps( json_list, indent=2 ), mimetype='application/json')
 
 
 @app.route('/servecatalog', methods=['GET'])
@@ -324,7 +330,8 @@ def serve_full_catalog():
     "# Generated: {}\n".format(strftime("%H:%M %B %d, %Y")) +\
     "#\n#  Mode is the set of observations used to fit the model\n" +\
     "#   0=SDSS+2MASS, 1=USNOB+2MASS\n"+\
-    "# " + "{}      {}       ".format("RA","DEC") + (' '*6).join(ALL_FILTERS) + " "*6 + '  '.join([val+"_err" for val in ALL_FILTERS]) + "  Mode\n"
+    "# " + "{}      {}       ".format("RA","DEC") + (' '*6).join(ALL_FILTERS) +\
+    " "*6 + '  '.join([val+"_err" for val in ALL_FILTERS]) + "  Mode\n"
     
     coll = DB[ session['sid'] ]
     curs = coll['data'].find()
@@ -352,10 +359,142 @@ def serve_zeropoints():
     for i in range(curs.count()):
         obj = curs.next()
         json_list.append( {'zp': obj['zp']} )
-    return Response(json.dumps( json_list ), mimetype='application/json')
+    return Response(json.dumps( json_list, indent=2 ), mimetype='application/json')
+
+
+@app.route('/api', methods=['GET','POST'])
+def api_handler():
+    '''
+    Creates and returns (in JSON or ASCII format) a catalog of sources in a field,
+     including both observed and modeled mags.  If fed an input file, attempts to 
+     process it and provide cross-matched models and zeropoint estimate as well.
+    If a 'GET' request, the url keys are: 'ra','dec','size', optional:'type'
+    If a 'POST', a properly-formatted file must be uploaded, and a passband url key
+     ('band') as well as a mode key ('mode') must be passed along.
+    '''
+    response_type = 'ascii' #what type of response to give. {'ascii', 'json'}
+    try:
+        typ = request.args.get('type')
+        if 'json' in typ.lower():
+            response_type = 'json'
+    except:
+        pass
+    if request.method == 'GET':
+        '''
+        Produce & return a catalog in a field.
+        '''
+        try:
+            ra = float( request.args.get('ra') )
+            dec = float( request.args.get('dec') )
+            size = float( request.args.get('size') )
+        except:
+            return Response( '{ success:false, message:"Request not formatted properly."}',
+                            mimetype='application/json')
+        if not (0. < ra < 360.) or not (-90. < dec < 90.) or not (0. < size < 7200.):
+            return Response( '{ success:false, message:"Requested field either too large or entered incorrectly."}',
+                            mimetype='application/json')
     
+        # create a database entry, so we can view these results through the GUI too
+        coll = create_collection()
+        coll.insert( {"entry":"mode", "mode":1} )
+        coll.insert( {"entry":"search_field", "ra":ra, "dec":dec, "fs":size} )
+        cat = gs.catalog( (ra,dec), size )
     
-    
+        # put the catalog entries both into the database and into a response
+        json_list = [ {'query_ID':session['sid']} ]
+        for i in range(len(cat.SEDs)):
+            coll['data'].insert( {"index":i, "sed":cat.SEDs[i].tolist(), "errors":cat.full_errors[i].tolist(),\
+                                    "mode":cat.modes[i], "coords":cat.coords[i].tolist(), "models":int(cat.models[i])} )
+            json_list.append({ 'ra':cat.coords[i][0], 'dec':cat.coords[i][1], 'mode':cat.modes[i], 'phot':cat.SEDs[i].tolist(),\
+                               'errors':cat.full_errors[i].tolist() })
+        # return the catalog in either ascii or JSON
+        if response_type == 'json':
+            return Response(json.dumps( json_list, indent=2 ), mimetype='application/json')
+        elif response_type == 'ascii':
+            ascii_out = build_ascii( json_list[1:], "Query ID: {}".format(json_list[0]['query_id']) )
+            response = Response(ascii_out, mimetype='text/plain')
+            response.headers['Content-Disposition'] = 'attachment; filename=catalog.txt'
+            return response
+    else:
+        '''
+        Produce and return a cross-matched catalog, perhaps with zeropoint estimate.
+        '''
+        try:
+            band = request.args.get('band')
+            mode = int( request.args.get('mode') )
+        except:
+            return Response( '{ success:false, message:"Request not formatted properly."}',
+                            mimetype='application/json')
+        if mode == 2:
+            # produce matched catalog only
+            source_file = request.files["source_file"]
+            if source_file and allowed_file(source_file.filename):
+                # try to parse with numpy
+                try:
+                    source_file.save( app.config['UPLOAD_FOLDER'] + 'tmp_source.txt' )
+                    data = np.loadtxt( app.config['UPLOAD_FOLDER'] + 'tmp_source.txt' )[:1000] #only accept first 1000 sources
+                except:
+                    return Response( '{ success:false, message:"Uploaded file incorrectly formatted."}',
+                                    mimetype='application/json')
+            else:
+                return Response( '{ success:false, message:"Uploaded file incorrectly formatted."}',
+                                mimetype='application/json')
+            # if all's good, create the collection and populate it
+            coll = create_collection()
+            coll.insert( {"entry":"mode", "mode":mode} )
+            for row in data:
+                coll["requested_sources"].insert( {"ra":row[0], "dec":row[1] })
+            
+            requested_coords = data[:,:2] #put ra,dec into numpy array for sake of functions below
+            
+            center, size = gs.find_field( requested_coords )
+            cat = gs.catalog( center, max(size) )
+            # match requested coords to returned sources
+            matches = gs.identify_matches( requested_coords, cat.coords)
+            out_coords, out_model_indices = [],[]
+            json_list = [ {'query_ID':session['sid']} ]
+            i = 0
+            for match in matches:
+                if match != None:
+                    coll['data'].insert( {"index":i, "sed":cat.SEDs[match].tolist(), "errors":cat.full_errors[match].tolist(),\
+                                            "mode":cat.modes[match], "coords":cat.coords[match].tolist(), "models":int(cat.models[match])} )
+                    out_model_indices.append( cat.models[match] )
+                    out_coords.append( cat.coords[match] )
+                    json_list.append({ 'ra':cat.coords[match][0], 'dec':cat.coords[match][1], 'mode':cat.modes[match], 'phot':cat.SEDs[match].tolist(),\
+                                       'errors':cat.full_errors[match].tolist() })
+                    i +=1
+            # return the catalog in either ascii or JSON
+            if response_type == 'json':
+                return Response(json.dumps( json_list, indent=2 ), mimetype='application/json')
+            elif response_type == 'ascii':
+                ascii_out = build_ascii( json_list[1:], "Query ID: {}".format(json_list[0]['query_id']) )
+                response = Response(ascii_out, mimetype='text/plain')
+                response.headers['Content-Disposition'] = 'attachment; filename=catalog.txt'
+                return response
+
+
+def build_ascii( json_list, header_str=None ):
+    '''
+    Helper function for API.
+    '''
+    ascii_out = \
+    "# Catalog produced by the Photometric Estimate Server\n"+\
+    "# http://classy.astro.berkeley.edu/ \n" +\
+    "# Generated: {}\n".format(strftime("%H:%M %B %d, %Y")) +\
+    "#\n#  Mode is the set of observations used to fit the model\n" +\
+    "#   0=SDSS+2MASS, 1=USNOB+2MASS\n"
+    if header_str != None:
+        ascii_out += '# '+header_str+'\n'
+    ascii_out += "# " + "{}       {}       ".format("RA","DEC") + (' '*6).join(ALL_FILTERS) + " "*6 +\
+    '  '.join([val+"_err" for val in ALL_FILTERS]) + "  Mode\n"
+    for obj in json_list:
+        ascii_out += " ".join(map(lambda x: "%.6f"%x, [obj["ra"], obj["dec"]]))+" "
+        ascii_out += " ".join(map(lambda x: "%.3f"%x, obj["phot"]))+" "
+        ascii_out += " ".join(map(lambda x: "%.4f"%x, obj["errors"]))+" "
+        ascii_out += str(obj["mode"])+"\n"
+    return ascii_out
+
+
 #######################################################################
 def mag2flam( magnitudes, bands ):
     '''
@@ -367,6 +506,7 @@ def mag2flam( magnitudes, bands ):
         flam[i] = f0*10**(-.4*magnitudes[i])
     return flam
 
+
 def flam2mag( flams, bands ):
     '''
     Converts flam back to magnitudes.
@@ -376,4 +516,6 @@ def flam2mag( flams, bands ):
         f0 = FILTER_PARAMS[b][1]
         mags[i] = -2.5*np.log10( flams[i]/f0 )
     return mags
+
+
 
